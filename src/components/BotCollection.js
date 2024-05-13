51f60b6f6dc4f77c556e8b0c3466bf65fb01b6c6
Molly <mollymuthama@gmail.com>
2024-05-13 11:38:59 +0300

    Modified bots page

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,17 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import BotSpecs from "./BotSpecs";
 //import BotCard from "./BotCard";
 
 function BotCollection(props) {
-  const bots = props
+  const { bots } = props
 
   return (
     <div className="ui four column grid">
       <div className="row">
         {bots.map((bot) => {
-           <BotSpecs 
+           return(
+            <BotSpecs 
               key={bot.id}
               bot={bot}/>
+           )
         })}
         Collection of all the bots.
       </div>
--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -4,31 +4,33 @@ import BotCollection from "./BotCollection";
 
 const url = "http://localhost:8002/bots";
 
-function BotsPage() {
+function BotsPage(
+) {
   const [bots, setBots] = useState([]);
   //const [botArmy, setBotArmy] = useState([]);
-
-  useEffect(() => {
-    const getBots = async () => {
-      const response = await fetch(url);
-
-      if (!response.ok) {
-        throw new Error(
-          "HTTP status " + response.status + ": " + response.statusText
-        )}
-
-      const data = await response.json();
-      console.log(data);
-      setBots(data);
-    };
-
+   useEffect(() => {
     getBots();
-  }, []);
+    }, []);
+
+  const getBots = () => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+  }
+  
+  fetch(url)
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result)
+      setBots(result)
+    })
+    .catch((error) => console.error(error));
 
   return (
     <div>
       <YourBotArmy />
-      <BotCollection bots={bots} />
+      <BotCollection
+        bots={bots}
+      />
     </div>
   );
 }
